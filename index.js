8ed6146ef5834ca5c4faaf0cbb2ac02dea15afe2
Neo Mosotho <156676402+neomosotho@users.noreply.github.com>
2024-04-12 15:25:45 +0200

    In progress

---
+++ index.js
@@ -0,0 +1,70 @@
+fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+    .then(res => res.json())
+    .then(data => {
+        document.body.style.backgroundImage = `url(${data.urls.regular})`
+		document.getElementById("author").textContent = `By: ${data.user.name}`
+    })
+    .catch(err => {
+
+        document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1462400362591-9ca55235346a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3wxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTI4MjU1Mjd8&ixlib=rb-4.0.3&q=80&w=1080
+)`
+		document.getElementById("author").textContent = `By:Kalen Emsley`
+    })
+
+    fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+    .then(res => {
+        if (!res.ok) {
+            throw Error("Something went wrong")
+        }
+        return res.json()
+    })
+
+    .then (data => {
+        document.getElementById("crypto-top").innerHTML = `
+        <img src=${data.image.small} />
+        <span>${data.name}</span>
+        `
+        document.getElementById("crypto").innerHTML += `
+        <p>🎯:R${data.market_data.current_price.zar}</p>
+        <p>👆:R${data.market_data.high_24h.zar}</p>
+        <p>👇:R${data.market_data.low_24h.zar}</p>
+        `
+    })
+    .catch(err => console.error(err))
+     
+function getCurrentTime() { 
+    const date = new Date() 
+    document.getElementById("time").textContent = date.toLocaleTimeString("en-us", {timeStyle: "medium"})
+}
+
+    setInterval(getCurrentTime, 1000) 
+
+    // navigator.geolocation.getCurrentPosition(position =>{
+    //     console.log(position)
+    // });
+
+    navigator.geolocation.getCurrentPosition(position => {
+        fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+        .then(res => {
+            if (!res.ok){
+                throw Error("Weather data not available")
+            }
+            return res.json()
+        })
+        .then(data => {
+            console.log(data)
+            const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+            document.getElementById("weather").innerHTML = `
+            <img src=${iconUrl} />
+            <p class="weather-temp">${Math.round(data.main.temp)}</p>
+            <p class="weather-city">${data.name}</p> `
+        })
+        .catch(err => console.error(err))
+    });
+
+
+
+
+
+
+ 
